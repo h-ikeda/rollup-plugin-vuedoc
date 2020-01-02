825efc205ebcb39954b7dacb4bd60cfcfbb1581f
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-01-02 12:19:12 +0900

    feat: index option (#3)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { md } = require('@vuedoc/md');
-const { dirname, resolve, join, parse, relative, format } = require('path');
+const { dirname, resolve, join, parse, relative, format, basename } = require('path');
 
 function match(target, test) {
   const arr = Array.isArray(test) ? test : [test];
@@ -10,9 +10,9 @@ function match(target, test) {
   }); 
 }
 
-module.exports = function vuedoc({ test, prefix = '', intro = '', outro = '' } = {}) {
+module.exports = function vuedoc({ test, prefix = '', intro = '', outro = '', index } = {}) {
 
-  if (!/function|string/.test(typeof intro) | !/function|string/.test(typeof outro)) {
+  if (!/function|string/.test(typeof intro) || !/function|string/.test(typeof outro)) {
     throw new TypeError('intro/outro option must be a function or a string.');
   }
 
@@ -20,8 +20,10 @@ module.exports = function vuedoc({ test, prefix = '', intro = '', outro = '' } =
     name: 'vuedoc',
     async buildStart({ input }) {
       this.vuedocInput = dirname(resolve(input));
+      this.vuedocIndices = [];
     },
     async transform(code, id) {
+      // Skip processing if ID does not match the test
       if (test !== undefined) {
         try {
           if (!match(id, test)) return null;
@@ -29,6 +31,7 @@ module.exports = function vuedoc({ test, prefix = '', intro = '', outro = '' } =
           this.error(e);
         }
       }
+      // Generate and emit markdown
       const doc = await md({ filename: id });
       if (!doc.length) return null;
       const introString = typeof intro === 'function' ? intro({ id }) : intro;
@@ -39,6 +42,20 @@ module.exports = function vuedoc({ test, prefix = '', intro = '', outro = '' } =
       const { dir, name } = parse(join(prefix, relative(this.vuedocInput, id)));
       const fileName = format({ dir, name, ext: '.md' });
       this.emitFile({ type: 'asset', source, fileName });
+      // Generate and emit index
+      if (!index) return null;
+      const emitIndex = (dirPath) => {
+        const indexPath = join(dirPath, 'index.md');
+        if (this.vuedocIndices.includes(indexPath)) return;
+        const indexContent = typeof index === 'function' ?
+          index(dirPath):
+          `# ${basename(dirPath || join(this.vuedocInput, prefix))}`;
+        this.emitFile({ type: 'asset', source: indexContent, fileName: indexPath});
+        this.vuedocIndices.push(indexPath);
+        if (dirPath === prefix) return;
+        emitIndex(dirname(dirPath));
+      }
+      emitIndex(dir);
       return null;
     },
   };
