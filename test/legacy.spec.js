1c091a0cf5270a3bb2c7495819025716df837b87
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-08-28 07:07:10 +0000

    update latest rollup version to 2

--- test/legacy.spec.js
+++ test/legacy.spec.js
@@ -1,4 +1,6 @@
 const { rollup } = require('rollup-1.19.2');
 const tests = require('./tests');
 
-describe('rollup-plugin-vuedoc with legacy rollup (v1.19.2)', tests(rollup));
+describe('rollup-plugin-vuedoc with legacy rollup (v1.19.2)', tests(rollup, {
+  assetObject: () => expect.objectContaining({ isAsset: true }),
+}));
